ed43e72a6191bac9ee4ecc4d4c39095476a9f3d7
umbertofaenza <umberto.faenza@gmail.com>
2023-09-29 15:42:07 +0200

    create new movie object and print their properties

--- index.php
+++ index.php
@@ -7,3 +7,15 @@ $movie_1 = new Movie(
     "1990",
     "drama"
 );
+
+$movie_2 = new Movie(
+    "one flew over the cuckoo's nest",
+    "1975",
+    "drama"
+);
+
+echo $movie_1->getCapitalisedTitle($movie_1->title);
+var_dump($movie_1);
+
+echo $movie_2->getCapitalisedTitle($movie_2->title);
+var_dump($movie_2);
